722c7749ff02e4f08abb9a74f1dcd05b4b0eee78
Orangehundred <87043291+Orangehundred@users.noreply.github.com>
2022-04-02 18:46:25 -0500

    Added HUD with heart image and score counter

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3,10 +3,13 @@ package;
 import flixel.FlxG;
 import flixel.FlxState;
 import player.Player;
+import Hud;
+import flixel.FlxCamera;
 
 class PlayState extends FlxState
 {
 	var player:Player;
+	var hud:Hud;
 
 	override public function create()
 	{
@@ -14,8 +17,11 @@ class PlayState extends FlxState
 
 		// create player
 		player = new Player(FlxG.width / 4, FlxG.height / 4);
+		hud = new Hud(player, 22, 22);
+
 
 		add(player);
+		add(hud);
 	}
 
 	override public function update(elapsed:Float)
