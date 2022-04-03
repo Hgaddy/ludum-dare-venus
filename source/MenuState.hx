359fb036355c5de859acca2506e7db5086822a01
Hgaddy <holdengaddy@gmail.com>
2022-04-03 00:57:46 -0500

    Added music to game.

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -9,66 +9,73 @@ import flixel.util.FlxColor;
 
 class MenuState extends FlxState
 {
-    var titleText:FlxText;
-    var titleText2:FlxText;
-    var playButton:FlxButtonPlus;
-    #if desktop
-    var exitButton:FlxButtonPlus;
-    #end
+	var titleText:FlxText;
+	var titleText2:FlxText;
+	var playButton:FlxButtonPlus;
+	#if desktop
+	var exitButton:FlxButtonPlus;
+	#end
 
-    override public function create()
-    {
-        // first part of title
-        titleText = new FlxText(20, 0, 0, "Space Saw", 35);
-        titleText.alignment = CENTER;
-        titleText.screenCenter(X);
-        add(titleText);
+	override public function create()
+	{
+		if (FlxG.sound.music == null) // don't restart the music if it's already playing
+		{
+			// start music
+			FlxG.sound.playMusic(AssetPaths.sawintro__wav, 1, true);
+		}
 
-        // second part of title
-        titleText2 = new FlxText(20, 60, 0, "Defense", 35);
-        titleText2.alignment = CENTER;
-        titleText2.screenCenter(X);
-        add(titleText2);
+		// first part of title
+		titleText = new FlxText(20, 0, 0, "Space Saw", 35);
+		titleText.alignment = CENTER;
+		titleText.screenCenter(X);
+		add(titleText);
 
-        // add play button
-        playButton = new FlxButtonPlus(0, 0, clickPlay, "Play", 200, 50);
-        //PlayButton.onUp.sound = FlxG.sound.load(AssetPaths.start__wav);
-        playButton.x = (FlxG.width / 2) - (0.5 * playButton.width);
-        playButton.y = FlxG.height - playButton.height - 10;
-        add(playButton);
+		// second part of title
+		titleText2 = new FlxText(20, 60, 0, "Defense", 35);
+		titleText2.alignment = CENTER;
+		titleText2.screenCenter(X);
+		add(titleText2);
 
-        // add exit button
-        #if desktop
-        exitButton = new FlxButtonPlus(FlxG.width - 90, 8, clickExit, "X", 80, 20);
-        //exitButton.loadGraphic(AssetPaths.button__png, true, 20, 20);
-        add(exitButton);
-        #end
+		// add play button
+		playButton = new FlxButtonPlus(0, 0, clickPlay, "Play", 200, 50);
+		// PlayButton.onUp.sound = FlxG.sound.load(AssetPaths.start__wav);
+		playButton.x = (FlxG.width / 2) - (0.5 * playButton.width);
+		playButton.y = FlxG.height - playButton.height - 10;
+		add(playButton);
 
-        FlxG.camera.fade(FlxColor.BLACK, 0.33, true);
+		// add exit button
+		#if desktop
+		exitButton = new FlxButtonPlus(FlxG.width - 90, 8, clickExit, "X", 80, 20);
+		// exitButton.loadGraphic(AssetPaths.button__png, true, 20, 20);
+		add(exitButton);
+		#end
 
-        super.create();
-    }
+		FlxG.camera.fade(FlxColor.BLACK, 0.33, true);
 
-    private function clickPlay()
-    {
-        FlxG.camera.fade(FlxColor.BLACK, 0.33, false, function()
-        {
-            FlxG.switchState(new PlayState());
-        });
-    }
+		super.create();
+	}
 
-    #if desktop
-    private function clickExit()
-    {
-        Sys.exit(0);
-    }
-    #end
+	private function clickPlay()
+	{
+		FlxG.camera.fade(FlxColor.BLACK, 0.33, false, function()
+		{
+			FlxG.sound.music.stop();
+			FlxG.switchState(new PlayState());
+		});
+	}
 
-    override public function update(elapsed:Float)
-    {
-        super.update(elapsed);
+	#if desktop
+	private function clickExit()
+	{
+		Sys.exit(0);
+	}
+	#end
 
-        if (FlxG.keys.justPressed.ENTER)
-            FlxG.fullscreen = !FlxG.fullscreen;
-    }
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		if (FlxG.keys.justPressed.ENTER)
+			FlxG.fullscreen = !FlxG.fullscreen;
+	}
 }
